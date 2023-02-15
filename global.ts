093a3f5ea498427c1a31236bda906ef0c0793461
Sergio Mallma <sergio.mallma876@gmail.com>
2023-02-14 23:13:48 -0500

    update route home

--- global.ts
+++ global.ts
@@ -1,6 +1,6 @@
 export const navbarRouter = [
   {
-    path: "/home",
+    path: "/",
     name: "Home",
   },
   {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
-import type { NextPage } from "next";
-import Head from "next/head";
+import { NextPage } from "next";
+import React from "react";
+import Picture from "../app/components/home/Picture/Picture";
+import Profile from "../app/components/home/Profile/Profile";
+import HomeLayout from "../components/Layouts/HomeLayout/HomeLayout";
 
 const Home: NextPage = () => {
   return (
-    <div>home</div>
+    <HomeLayout leftSide={<Profile />}>
+      <Picture />
+    </HomeLayout>
   );
 };
 
