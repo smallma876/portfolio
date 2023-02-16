169c9187fb5f9ab698018652dd9d78d05938ea5c
Sergio Mallma <sergio.mallma876@gmail.com>
2023-02-15 19:34:39 -0500

    icons language programming added

--- app/components/home/Profile/Profile.tsx
+++ app/components/home/Profile/Profile.tsx
@@ -1,13 +1,57 @@
+import Image from "next/image";
 import React from "react";
 import GiantText from "../../../../components/GiantText/GiantText";
 import TypingEffect from "../../../../components/TypingEffect/TypingEffect";
+import { Box, Flexbox } from "../../../../shared/components/Flexbox/Flexbox";
 import { Palette } from "../../../../utilities/palette";
+
+const languagesProgramming = [
+  {
+    src: "./images/it/css3-icon.svg",
+    alt: "css3",
+  },
+  {
+    src: "./images/it/html-icon.svg",
+    alt: "html",
+  },
+  {
+    src: "./images/it/javascript-icon.svg",
+    alt: "javascript",
+  },
+  {
+    src: "./images/it/reactjs-icon.svg",
+    alt: "reactjs",
+  },
+  {
+    src: "./images/it/angular-icon.svg",
+    alt: "angular",
+  },
+  {
+    src: "./images/it/typescript-icon.svg",
+    alt: "typescript",
+  },
+  {
+    src: "./images/it/nextjs-icon.svg",
+    alt: "nextjs",
+  },
+  {
+    src: "./images/it/git-icon.svg",
+    alt: "git",
+  },
+];
 
 const Profile = () => {
   return (
     <>
       <TypingEffect component="h1" variant="h1" text="I'm Sergio" />
       <GiantText color={Palette.DarkYellow}>Front End Developer</GiantText>
+      <Flexbox gap="0.5rem">
+        {languagesProgramming.map(({ src, alt }) => (
+          <Box key={alt}>
+            <Image src={src} alt={alt} width={30} height={30} />
+          </Box>
+        ))}
+      </Flexbox>
     </>
   );
 };
